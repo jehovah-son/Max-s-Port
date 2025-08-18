83717c1bf7a46fa71326ba919980a7fa007c86c5
jehovah-son <chukwukasuccess0@gmail.com>
2025-08-18 02:28:18 +0100

    `Refactor categories page to use Next.js navigation and data fetching`

---
+++ src/app/portfolio/[category]/data.ts
@@ -0,0 +1,22 @@
+
+export const Frameworks = {
+    nextjs: [
+        {
+            id: 1,
+            title: "Sancus africa",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "/next-logo.png",
+            link: "https://nextjs.org/"
+        }
+    ],
+    react: [
+        {
+            id: 1,
+            title: "Todo app",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "/react-logo.png",
+            link: "https://react.dev"
+
+        }
+    ]
+}
--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,5 +1,43 @@
-import React from "react";
+import { notFound } from "next/navigation";
+import { Frameworks } from "./data";
+import Image from "next/image";
 
-export default function categoriesPage() {
-  return <div>categoriesPage</div>;
+// define a type from the keys of Frameworks
+// type FrameworkKey = keyof typeof Frameworks;
+
+const getData = (cat: string) => {
+  const data = Frameworks[cat as keyof typeof Frameworks];
+
+  if (data) {
+    return data;
+  } else {
+    return notFound();
+  }
+};
+
+type PageProps = {
+  params: {
+    category: string;
+  };
+};
+
+export default async function CategoriesPage({ params }: PageProps) {
+  // Fetch the data based on the category
+  console.log(params);
+  const data = getData(params.category);
+
+  // If data is found, return the JSX
+  return (
+    <div>
+      <h1>{params.category}</h1>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+          <Image src={item.image} alt={item.title} width={200} height={200} />
+          <p>{item.link}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
