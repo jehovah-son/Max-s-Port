0bb10d1a59b074fc2aa5350a5e6b15a5b2dc0bb9
jehovah-son <chukwukasuccess0@gmail.com>
2025-08-18 13:14:50 +0100

    added more data

--- src/app/portfolio/[category]/data.ts
+++ src/app/portfolio/[category]/data.ts
@@ -18,5 +18,23 @@ export const Frameworks = {
             link: "https://react.dev"
 
         }
+    ],
+    typescript: [
+        {
+            id: 1,
+            title: "typescript",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "/typescript-logo.png",
+            link: "https://typescript.dev"
+        }
+    ],
+    flutter: [
+        {
+            id: 1,
+            title: "flutter",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            image: "/flutter-logo.png",
+            link: "https://flutter.dev"
+        }
     ]
 }
