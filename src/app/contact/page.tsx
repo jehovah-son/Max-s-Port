ddf7da9972eb0279547ee68cbd9ac91ef03c1835
jehovah-son <chukwukasuccess0@gmail.com>
2025-08-19 23:28:56 +0100

    `Added contact page with image and form functionality`

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
 import Loading from "@/app/contact/loading";
+import Button from "@/component/button/Button";
+import Image from "next/image";
+import ContactImage from "@/../public/contact.png";
 
 export default function Contact() {
   const [isLoading, setIsLoading] = useState(true);
@@ -16,5 +19,43 @@ export default function Contact() {
   if (isLoading == true) {
     return <Loading />;
   }
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div>
+        <h1>Let&apos;s Keep in Touch</h1>
+      </div>
+      <div className="flex md:justify-between  pt-14 md:flex-row flex-col ">
+        {/* image */}
+        <div className="flex-1">
+          <Image
+            src={ContactImage}
+            alt="Contact Image"
+            width={450}
+            height={500}
+            className="object-cover"
+            priority
+          />
+        </div>
+        <div className="flex-1">
+          <form className="flex flex-col gap-5">
+            <input
+              type="text"
+              placeholder="Name"
+              className="border-2 border-gray-400"
+              required
+            />
+            <input type="email" placeholder="Email" className="" required />
+            <textarea
+              rows={10}
+              cols={30}
+              placeholder="Message"
+              className=""
+              required
+            ></textarea>
+            <Button href="/" text="Send Message" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
