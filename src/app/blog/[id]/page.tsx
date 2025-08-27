0c62be7773c0183b9aee4cc313d9fe978ac43c98
jehovah-son <chukwukasuccess0@gmail.com>
2025-08-27 18:19:17 +0100

    `Refactor layout and styling of blog page with [id]`

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -7,11 +7,11 @@ export default async function PageId({ params }: any) {
   const data = await BlogDataId(params.id);
   return (
     <div className="pb-10">
-      <div className="flex justify-between">
+      <div className="flex md:justify-between gap-10 flex-col-reverse md:flex-row">
         {/* title desc username */}
         <div className="flex-1 flex-col flex justify-between">
-          <p className="text-4xl font-bold">{data.title}</p>
-          <p>{data.desc}</p>
+          <p className="md:text-4xl text-2xl font-bold ">{data.title}</p>
+          <p className="md:py-0 py-5">{data.desc}</p>
           <div className="flex gap-3 items-center">
             <div className="w-[35px] h-[35px] rounded-full overflow-hidden relative">
               <Image
@@ -32,14 +32,14 @@ export default async function PageId({ params }: any) {
             alt="title img"
             width={1000}
             height={1000}
-            className="w-full h-full display-contain"
+            className="w-full h-full display-contain "
             priority
           />
         </div>
       </div>
       {/* text content */}
       <div className="pt-16">
-        <p>{data.content}</p>
+        <p className="text-justify">{data.content}</p>
       </div>
     </div>
   );
